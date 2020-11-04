f760b6c01944da77fef6f242dcadbe344792d6f1
comp211 <ferdmaa@post.bgu.ac.il>
2020-11-04 20:27:21 +0200

    added parsers

--- reader.ml
+++ reader.ml
@@ -49,19 +49,45 @@ let p_digit = range '0' '9' ;;
 let p_natural = plus p_digit ;; 
 
 (* char parsers *)
+let p_dollar = char '$' ;;
+let p_exclamation = char '!' ;;
+let p_caret = char '^' ;;
+let p_star = char '*' ;;
+let p_minus = char '-' ;;
+let p_underline = char '_' ;;
 let p_equal = char '=' ;;
+let p_plus = char '+' ;;
+let p_bigger = char '>' ;;
+let p_smaller = char '<' ;;
+let p_question = char '?' ;;
+let p_colon = char '=' ;;
 let p_dot = char '.' ;; 
+let p_f = char_ci 'f' ;;
+let p_t = char_ci 't' ;;
+let p_slash = char '\\'
+let p_newline = word "newline" ;;
+let p_nul = word "nul" ;;
+let p_page = word "page" ;;
+let p_return = word "return" ;;
+let p_space = word "space" ;;
+let p_tab = word "tab" ;;
 let p_Lparentheses1 = char '(' ;;
 let p_Rparentheses1 = char ')' ;;
 let p_Lparentheses2 = char '{' ;;
 let p_Rparentheses2 = char '}' ;;
-
-(* quotations parsers *)
+let p_booleanF = caten p_hashtag p_f ;;
+let p_booleanT = caten p_hashtag p_t ;;
+let p_charPref = caten p_hashtag p_slash ;;
 let p_quote = char '\'' ;;
 let p_Qquote = char '`' ;;
 let p_Uquote = char ',' ;;
 let p_UASquote = word_ci ",@" ;;
 
+(* compositions *)
+let p_lower = range 'a' 'z' ;;
+let p_upper = range 'A' 'Z' ;;
+let p_NumSign = disj p_plus p_minus
+let p_integer = caten ( maybe p_NumSign ) ;;
 (* end of my code *)
 
 let read_sexprs string = raise X_not_yet_implemented;;
